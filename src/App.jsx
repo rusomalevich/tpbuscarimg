75a2ef4c276cc80daf2dfb923c8ff319978394e8
Ezequiel ADIUNGS <ezequiel.glik@gmail.com>
2023-02-08 17:35:06 -0300

    conexion andando

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
+import axios from 'axios'
 
 function App() {
+
+  const apiKey = 'kGS0Yw08xWCh64TiRy_U421UZRvGQhnmYWYuoIPWAs8'
+  const BASEurl = 'https://api.unsplash.com/search/photos/'
+  const [imagenes, setImagenes]=useState([])
+  const busqueda = 'keyboards'
+  
+  useEffect(()=>{
+    const obtenerImagenes=async ()=> {
+      const response = await axios.get(`${BASEurl}?client_id=${apiKey}&query=${busqueda}`)
+      console.log(response)
+    }
+    obtenerImagenes()
+  }
+
+  )
 
   return (
     <div>
