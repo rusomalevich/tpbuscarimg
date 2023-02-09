94e59ec28a615cf09076c75a5d7ace46f5b70333
Ezequiel ADIUNGS <ezequiel.glik@gmail.com>
2023-02-09 15:07:48 -0300

    Quedó funcionando la búsqueda

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import './App.css'
 import axios from 'axios'
 
@@ -7,23 +7,38 @@ function App() {
   const apiKey = 'kGS0Yw08xWCh64TiRy_U421UZRvGQhnmYWYuoIPWAs8'
   const BASEurl = 'https://api.unsplash.com/search/photos/'
   const [imagenes, setImagenes]=useState([])
-  const busqueda = 'keyboards'
+  const busquedaRef=useRef(null)
+  const [busqueda, setBusqueda] = useState('search')
+
   
   useEffect(()=>{
     const obtenerImagenes=async ()=> {
+      //const response = await axios.get(`${BASEurl}?client_id=${apiKey}&query=keyboards`)
       const response = await axios.get(`${BASEurl}?client_id=${apiKey}&query=${busqueda}`)
+      const propLimite = 'x-ratelimit-remaining'
+      const limite = response.headers[propLimite]
+      console.log(`${limite} consultas disponibles`)
       const datos=response.data
+      console.log(response)
       setImagenes(datos.results)
     }
     obtenerImagenes()
-  }, []
-    
-  )
+  }, [busqueda])
+    const buscar=()=>{
+      setBusqueda(busquedaRef.current.value)
+    }
 
   return (
     <div>
       <h1>Holus</h1>
-      {imagenes.map((elemento) => <h2>{elemento.alt_description}</h2>)}
+      <input
+        ref={busquedaRef}
+        type='search'
+        placeholder='Buscar imágenes'
+        name='buscar'
+        />
+      <button onClick={() => buscar()}>Buscar</button>
+      <div>{imagenes.map((elemento) => <img key={elemento.id} src={elemento.urls.small_s3} alt={elemento.alt_description} />)}</div>
     </div>
   )
 }
