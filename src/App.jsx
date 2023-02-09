42f7a6af9550684ea59891710cad05e1ce71cbd8
Ezequiel ADIUNGS <ezequiel.glik@gmail.com>
2023-02-09 18:24:50 -0300

    empieza random y sigue según búsqueda. Agrego algunas propiedades

--- src/App.jsx
+++ src/App.jsx
@@ -5,26 +5,40 @@ import axios from 'axios'
 function App() {
 
   const apiKey = 'kGS0Yw08xWCh64TiRy_U421UZRvGQhnmYWYuoIPWAs8'
-  const BASEurl = 'https://api.unsplash.com/search/photos/'
+  //const BASEurl = 'https://api.unsplash.com/photos/random/'
+  const BASEurl = 'https://api.unsplash.com/'
+  const apiSearch = 'search/photos/'
+  const apiRandom = 'photos'
   const [imagenes, setImagenes]=useState([])
   const busquedaRef=useRef(null)
-  const [busqueda, setBusqueda] = useState('search')
+  const [busqueda, setBusqueda] = useState(null)
 
   
   useEffect(()=>{
     const obtenerImagenes=async ()=> {
-      //const response = await axios.get(`${BASEurl}?client_id=${apiKey}&query=keyboards`)
-      const response = await axios.get(`${BASEurl}?client_id=${apiKey}&query=${busqueda}`)
-      const propLimite = 'x-ratelimit-remaining'
-      const limite = response.headers[propLimite]
-      console.log(`${limite} consultas disponibles`)
-      const datos=response.data
-      console.log(response)
-      setImagenes(datos.results)
+      
+      if(busqueda==null){
+        const response = await axios.get(`${BASEurl}${apiRandom}?client_id=${apiKey}`)
+        const propLimite = 'x-ratelimit-remaining'
+        const limite = response.headers[propLimite]
+        const datos = response.data
+        setImagenes(datos)
+        console.log(`${limite} consultas disponibles`)
+        console.log(response)
+      } else {
+        const response = await axios.get(`${BASEurl}${apiSearch}?client_id=${apiKey}&query=${busqueda}`)
+        const propLimite = 'x-ratelimit-remaining'
+        const limite = response.headers[propLimite]
+        const datos = response.data
+        setImagenes(datos.results)
+        console.log(`${limite} consultas disponibles`)
+        console.log(response)
+      }
     }
     obtenerImagenes()
   }, [busqueda])
     const buscar=()=>{
+      console.log('buscar')
       setBusqueda(busquedaRef.current.value)
     }
 
@@ -38,7 +52,19 @@ function App() {
         name='buscar'
         />
       <button onClick={() => buscar()}>Buscar</button>
-      <div>{imagenes.map((elemento) => <img key={elemento.id} src={elemento.urls.small_s3} alt={elemento.alt_description} />)}</div>
+      <div>{imagenes.map((elemento) => 
+        <div 
+          key={elemento.id}>
+            <img 
+              src={elemento.urls.small_s3} 
+              alt={elemento.alt_description} 
+            />
+            <p>{/*agregar condicional para que no renderice el P si no hay location*/}
+              {elemento.user.location}
+            </p>
+        </div>
+        )}
+      </div>
     </div>
   )
 }
